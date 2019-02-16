4033474871516a727d1415550a4ecc164435e0cf
jorge perez <jorgeperez00@hotmail.com>
2019-02-16 09:24:23 -0500

    fix bug with sleep

--- myshutdown/Program.cs
+++ myshutdown/Program.cs
@@ -22,16 +22,20 @@ namespace myshutdown {
                 shutDownType = shutDownTypeEnum.sleep;
             }
             switch (shutDownType) {
-                case shutDownTypeEnum.hybernate:
+                case shutDownTypeEnum.sleep:
+                    // arg = 0
                     Application.SetSuspendState(PowerState.Suspend, true, true);
                     break;
                 case shutDownTypeEnum.shutdown:
+                    // arg = 1
                     System.Diagnostics.Process.Start("shutdown.exe", "-s 0");
                     break;
                 case shutDownTypeEnum.restart:
+                    // arg = 2
                     System.Diagnostics.Process.Start("shutdown.exe", "-r");
                     break;
                 default:
+                    // arg >= 3
                     Application.SetSuspendState(PowerState.Hibernate, true, true);
                     break;
             }
